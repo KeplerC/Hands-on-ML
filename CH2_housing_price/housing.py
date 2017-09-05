4079aac775202ea8fc0e1e4e04b0ae7f2fd98aad
Kaiyuan <chenkaiyuan@ucla.edu>
2017-09-05 06:53:54 -0700

    Still-Have-Bug-In-Pipeline

--- CH2_housing_price/housing.py
+++ CH2_housing_price/housing.py
@@ -63,8 +63,16 @@ def give_a_quick_view(data):
     #data.hist(bins=50, figsuze=(20, 15))
     #plt.show()
 
+from sklearn.model_selection import StratifiedShuffleSplit
 def generating_test_set(data):
-    train_set, test_set = train_test_split(data)
+    data["income_cat"] = np.ceil(data["median_income"]/1.5)
+    data["income_cat"].where(data["income_cat"] < 5, 5.0, inplace=True)
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(data, data["income_cat"]):
+        train_set = data.loc[train_index]
+        test_set = data.loc[test_index]
+    for set in (train_set, test_set):
+        set.drop(["income_cat"], axis=1, inplace=True)
     return train_set, test_set
 
 def visualization(housing):
@@ -83,6 +91,26 @@ def data_cleaning(data):
     X = imputer.transform(data_num)
     return pd.DataFrame(X, columns = data_num.columns)
 
+#custom transformers 
+from sklearn.base import BaseEstimator, TransformerMixin
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+            return self # nothing else to do
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+
+
+from sklearn.base import BaseEstimator, TransformerMixin
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
@@ -90,14 +118,22 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
         return self
     def transform(self, X):
         return X[self.attribute_names].values
-    
-def pipeline(data):
-    num_attribs = list(data)
+
+from sklearn.preprocessing import LabelEncoder
+
+def pipeline(housing):
+    #handing text attributes 
+    housing_num = housing.drop("ocean_proximity", axis = 1)
+    num_attribs = list(housing_num)
     cat_attribs = ["ocean_proximity"]
-    num_pipeline = Pipeline([('imputer', Imputer(strategy="median")),('std_scaler', StandardScaler()),])
-    cat_pipeline = Pipeline([('selector', DataFrameSelector(cat_attribs)),('label_binarizer', LabelBinarizer()),])
+    num_pipeline = Pipeline([
+        ('selector', DataFrameSelector(num_attribs)),
+        ('imputer', Imputer(strategy="median")),('attribs_adder', CombinedAttributesAdder()),('std_scaler', StandardScaler()),])
+    cat_pipeline = Pipeline([
+        ('selector', DataFrameSelector(cat_attribs)),
+        ('label_binarizer', LabelBinarizer()),])
     full_pipeline = FeatureUnion(transformer_list=[("num_pipeline", num_pipeline),("cat_pipeline", cat_pipeline),])
-    return num_pipeline.fit_transform(data)
+    housing_prepared = full_pipeline.fit_transform(housing)
 
 from sklearn.metrics import mean_squared_error
 def error(model, test_set, labels):
@@ -105,16 +141,32 @@ def error(model, test_set, labels):
     lin_mse = mean_squared_error(labels, predictions)
     return np.sqrt(lin_mse)
 
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
 def main():
     housing = load_housing_data()
     #give_a_quick_view(housing)
     train_set, test_set = generating_test_set(housing)
     #visualization(train_set)
+    housing = train_set.drop("median_house_value", axis=1)
+    housing_labels = train_set["median_house_value"].copy()
     #print(correlation(housing, "median_house_value"))
-    data_cleaning(housing)
+    #data_cleaning(housing)
     #housing = pipeline(housing)
-    lin_reg = LinearRegression()
+    #lin_reg = LinearRegression()
     #lin_reg.fit(housing, housing["median_house_value"])
+    housing_num = housing.drop("ocean_proximity", axis = 1)
+    num_attribs = list(housing_num)
+    cat_attribs = ["ocean_proximity"]
+    num_pipeline = Pipeline([
+        ('selector', DataFrameSelector(num_attribs)),
+        ('imputer', Imputer(strategy="median")),('attribs_adder', CombinedAttributesAdder()),('std_scaler', StandardScaler()),])
+    cat_pipeline = Pipeline([
+        ('selector', DataFrameSelector(cat_attribs)),
+        ('label_binarizer', LabelBinarizer()),])
+    full_pipeline = FeatureUnion(transformer_list=[("num_pipeline", num_pipeline),("cat_pipeline", cat_pipeline),])
+    housing_prepared = full_pipeline.fit_transform(housing.ix[1:,:],housing.ix[1,:])
     
 if __name__ == "__main__":
     main()
