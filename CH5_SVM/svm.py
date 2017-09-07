b983c1e1911900a3b3532f5e398da51d49dbe7a2
Kaiyuan <chenkaiyuan@ucla.edu>
2017-09-07 05:50:42 -0700

    097

---
+++ CH5_SVM/svm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import numpy as np
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+iris = datasets.load_iris()
+X = iris["data"][:, (2, 3)] # petal length, petal width
+y = (iris["target"] == 2).astype(np.float64) # Iris-Virginica
+svm_clf = Pipeline((
+    ("scaler", StandardScaler()),
+    ("linear_svc", LinearSVC(C=1, loss="hinge")),
+))
+svm_clf.fit(X_scaled, y)
